5a9b7864edf76c0bee79889cc69d45fe315bfcf5
Timo Mäntykivi <9353866+tmantyki@users.noreply.github.com>
2024-12-26 15:41:29 +0200

    use exceptions for error handling

--- src/utility/geometry_importer.cpp
+++ src/utility/geometry_importer.cpp
@@ -5,91 +5,115 @@
 #include "geometry/vertex.hpp"
 #include "utility/geometry_importer.hpp"
 
-GeometryImporter::GeometryImporter(Space& space) : space_(space) {}
+GeometryImportException::GeometryImportException(const char* error_message)
+    : error_message_(error_message) {}
 
-ObjGeometryImporter::ObjGeometryImporter(Space& space)
-    : GeometryImporter(space), triangle_counter_(0), vertex_counter_(0) {}
-
-bool ObjGeometryImporter::ImportGeometryFromFile(const char* filename) {
-  std::ifstream input_file;
-  std::string line;
-  input_file.open(filename);
-  if (!input_file.is_open()) {
-    std::cout << "Error: could not open file '" << filename << "'";
-    return false;
-  }
-  while (std::getline(input_file, line)) {
-    if (!ParseLine(line)) {
-      input_file.close();
-      return false;
-    }
-  }
-  input_file.close();
-  space_.UpdateSpace();
-  std::cout << "Successfully read " << vertex_counter_ << " vertices and "
-            << triangle_counter_ << " triangles from " << filename << ".\n";
-  return true;
+const char* GeometryImportException::what() const noexcept {
+  return error_message_;
 }
 
-bool ObjGeometryImporter::ParseLine(const std::string& line) {
+InvalidFileException::InvalidFileException(const char* error_message)
+    : GeometryImportException(error_message) {}
+
+UnknownCommandException::UnknownCommandException(const char* error_message)
+    : GeometryImportException(error_message) {}
+
+WorldLimitsExceededException::WorldLimitsExceededException(
+    const char* error_message)
+    : GeometryImportException(error_message) {}
+
+MalformedParametersException::MalformedParametersException(
+    const char* error_message)
+    : GeometryImportException(error_message) {}
+
+UnsupportedPrimitiveException::UnsupportedPrimitiveException(
+    const char* error_message)
+    : GeometryImportException(error_message) {}
+
+GeometryImporter::GeometryImporter(Space& space)
+    : space_(space), triangle_counter_(0), vertex_counter_(0) {}
+
+void GeometryImporter::ImportGeometryFromFile(const char* filename) {
+  std::ifstream input_file_stream;
+  input_file_stream.open(filename);
+  if (!input_file_stream)
+    throw InvalidFileException("File could not be opened!");
+  ImportGeometryFromInputStream(input_file_stream);
+  input_file_stream.close();
+  std::cout << "Successfully read " << vertex_counter_ << " vertices and "
+            << triangle_counter_ << " triangles from " << filename << ".\n";
+}
+
+size_t GeometryImporter::GetTriangleCount() const noexcept {
+  return triangle_counter_;
+}
+
+size_t GeometryImporter::GetVertexCount() const noexcept {
+  return vertex_counter_;
+}
+
+ObjGeometryImporter::ObjGeometryImporter(Space& space)
+    : GeometryImporter(space) {}
+
+void ObjGeometryImporter::ImportGeometryFromInputStream(
+    std::istream& input_stream) {
+  std::string line;
+  while (std::getline(input_stream, line))
+    ParseLine(line);
+  space_.UpdateSpace();
+}
+
+void ObjGeometryImporter::ParseLine(const std::string& line) {
   std::stringstream ss(line);
   ss >> std::ws;
   std::string word;
   if (std::getline(ss, word, ' ')) {
+    if (word[0] == '#')
+      return;
     if (word == "v") {
-      return ParseVertex(ss);
+      ParseVertex(ss);
+      return;
     } else if (word == "f") {
-      return ParseFace(ss);
-    } else {
-      std::cout << "Error: unknown command '" << word << "'\n";
-      return false;
-    }
+      ParseFace(ss);
+      return;
+    } else
+      throw UnknownCommandException("Unknown command!");
   }
-  return true;  // empty lines return true
 }
 
-bool ObjGeometryImporter::ParseVertex(std::stringstream& vertex_params) {
+void ObjGeometryImporter::ParseVertex(std::stringstream& vertex_params) {
   std::array<float, kDimensions> read_value;
-  if (vertex_counter_ == kMaxVertices) {
-    std::cout << "Error: number of vertices exceeds " << kMaxVertices << ".\n";
-    return false;
-  }
+  if (vertex_counter_ == kMaxVertices)
+    throw WorldLimitsExceededException("Number of maximum vertices exceeded!");
   for (size_t dim = 0; dim < kDimensions; dim++) {
     vertex_params >> std::ws;
     if (!vertex_params.eof()) {
       vertex_params >> read_value[dim];
     } else {
-      assert(dim == 3);  // #TODO better error handling
+      assert(dim == 3);
       read_value[3] = 1.0;
     }
   }
   vertex_params >> std::ws;
-  if (!vertex_params.eof()) {
-    std::cout << "Error: vertex contained more than four coordinates.\n";
-    return false;
-  }
+  if (!vertex_params.eof())
+    throw MalformedParametersException(
+        "Vertex cointained more than four coordainates!");
   vertices_[vertex_counter_++] =
       Vector4(read_value[0], read_value[1], read_value[2], read_value[3]);
-  return true;
 }
 
-bool ObjGeometryImporter::ParseFace(std::stringstream& face_params) {
+void ObjGeometryImporter::ParseFace(std::stringstream& face_params) {
   std::array<size_t, kVerticesPerTriangle> read_index;
-  if (triangle_counter_ == kMaxTriangles) {
-    std::cout << "Error: number of triangles exceeds " << kMaxTriangles
-              << ".\n";
-    return false;
-  }
+  if (triangle_counter_ == kMaxTriangles)
+    throw WorldLimitsExceededException("Number of maximum triangles exceeded!");
   for (size_t vertex_i = 0; vertex_i < kVerticesPerTriangle; vertex_i++) {
     face_params >> std::ws;
     if (!face_params.eof()) {
       face_params >> read_index[vertex_i];
       assert(read_index[vertex_i] > 0);
       assert(read_index[vertex_i] <= vertex_counter_);
-    } else if (vertex_i != 3) {
-      std::cout << "Error: only triangle faces are supported.\n";
-      return false;
-    }
+    } else if (vertex_i != 3)
+      throw UnsupportedPrimitiveException("Only triangle faces are supported!");
   }
   // OBJ uses CCW winding by default
   Vertex& vertex_1 = vertices_[read_index[0] - 1];
@@ -101,5 +125,4 @@ bool ObjGeometryImporter::ParseFace(std::stringstream& face_params) {
       std::make_shared<Triangle>(vertex_1, vertex_2, vertex_3);
   space_.EnqueueAddTriangle(triangle);
   triangle_counter_++;
-  return true;
 }
--- src/utility/geometry_importer.hpp
+++ src/utility/geometry_importer.hpp
@@ -3,27 +3,65 @@
 
 #include "geometry/space.hpp"
 
+class GeometryImportException : public std::exception {
+ public:
+  GeometryImportException(const char* error_message);
+  virtual const char* what() const noexcept override;
+
+ private:
+  const char* error_message_;
+};
+
+class InvalidFileException : public GeometryImportException {
+ public:
+  InvalidFileException(const char* error_message);
+};
+
+class UnknownCommandException : public GeometryImportException {
+ public:
+  UnknownCommandException(const char* error_message);
+};
+
+class WorldLimitsExceededException : public GeometryImportException {
+ public:
+  WorldLimitsExceededException(const char* error_message);
+};
+
+class MalformedParametersException : public GeometryImportException {
+ public:
+  MalformedParametersException(const char* error_message);
+};
+
+class UnsupportedPrimitiveException : public GeometryImportException {
+ public:
+  UnsupportedPrimitiveException(const char* error_message);
+};
+
 class GeometryImporter {
  public:
   GeometryImporter(Space& space);
-  virtual bool ImportGeometryFromFile(const char* filename) = 0;
+  void ImportGeometryFromFile(const char* filename);
+  virtual void ImportGeometryFromInputStream(std::istream& input_stream) = 0;
+  size_t GetTriangleCount() const noexcept;
+  size_t GetVertexCount() const noexcept;
 
  protected:
   std::array<Vertex, kMaxVertices> vertices_;
   Space& space_;
+  size_t triangle_counter_;
+  size_t vertex_counter_;
 };
 
 class ObjGeometryImporter : public GeometryImporter {
  public:
   ObjGeometryImporter(Space& space);
-  virtual bool ImportGeometryFromFile(const char* filename) override;
+  virtual void ImportGeometryFromInputStream(
+      std::istream& input_stream) override;
 
  private:
-  bool ParseLine(const std::string& line);
-  bool ParseVertex(std::stringstream& vertex_params);
-  bool ParseFace(std::stringstream& face_params);
-  size_t triangle_counter_;
-  size_t vertex_counter_;
+  void ParseLine(const std::string& line);
+  void ParseVertex(std::stringstream& vertex_params);
+  void ParseFace(std::stringstream& face_params);
 };
 
 #endif
